4d0250cdf3bb224e1273a19a4a3489e30f28080d
Yurii <wervmer>
2023-03-20 22:57:13 +0200

    third commit

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,15 +1,17 @@
 public class StringCalculator {
 
+
     public static int add(String numbers) {
-        if (numbers.isEmpty())
+        if (numbers.equals("")) {
             return 0;
-        return sum(numbers.split(","));
+        }
+        return sum(numbers.split(",|\n"));
     }
 
     private static int sum(String[] numbers) {
         int sum = 0;
         for (String num: numbers) {
-            sum += Integer.parseInt(num);
+            sum += Integer.parseInt(num.equals("") ? "0" : num);
         }
         return sum;
     }
--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -25,4 +25,10 @@ class StringCalculatorTest {
         assertEquals(55, StringCalculator.add("1,2,3,4,5,6,7,8,9,10"));
     }
 
+    @Test
+    void comma_or_newLine_separators_ok() {
+        assertEquals(6, StringCalculator.add("1\n2,3"));
+        assertEquals(10, StringCalculator.add("1,5\n4"));
+    }
+
 }
